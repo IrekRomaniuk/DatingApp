ea8eb19e25c24868be4cbc36743e4ebf7b70d37c
Irek Romaniuk <irekromaniuk@Ireks-Mac-mini.local>
2019-12-27 11:07:07 -0500

    Doc Updated

--- DatingApp.API/Migrations/20191227155633_InitialCreate.Designer.cs
+++ DatingApp.API/Migrations/20191227155633_InitialCreate.Designer.cs
@@ -8,7 +8,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DatingApp.API.Migrations
 {
     [DbContext(typeof(DataContext))]
-    [Migration("20191226142931_InitialCreate")]
+    [Migration("20191227155633_InitialCreate")]
     partial class InitialCreate
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
@@ -23,8 +23,8 @@ namespace DatingApp.API.Migrations
                         .ValueGeneratedOnAdd()
                         .HasColumnType("INTEGER");
 
-                    b.Property<int>("Name")
-                        .HasColumnType("INTEGER");
+                    b.Property<string>("Name")
+                        .HasColumnType("TEXT");
 
                     b.HasKey("Id");
 
